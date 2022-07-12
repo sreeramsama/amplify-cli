4f061a836af3b76cd3ef996f77c3b9e232e54a77
lazpavel <85319655+lazpavel@users.noreply.github.com>
2022-07-11 21:37:05 -0400

    fix: enforce init command to be executed in project root

--- packages/amplify-cli-core/src/state-manager/pathManager.ts
+++ packages/amplify-cli-core/src/state-manager/pathManager.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs-extra';
 import { homedir } from 'os';
 import { NotInitializedError } from '../errors';
 // eslint-disable-next-line import/no-cycle
-import { overriddenCategories } from '..';
+import { overriddenCategories, stateManager } from '..';
 
 export const PathConstants = {
   // in home directory
@@ -280,8 +280,11 @@ export class PathManager {
     if (fs.existsSync(projectPath)) {
       const amplifyDirPath = this.getAmplifyDirPath(projectPath);
       const dotConfigDirPath = this.getDotConfigDirPath(projectPath);
+      const localEnvFilePath = this.getLocalEnvFilePath(projectPath);
 
-      return fs.existsSync(amplifyDirPath) && fs.existsSync(dotConfigDirPath);
+      if (fs.existsSync(amplifyDirPath) && fs.existsSync(dotConfigDirPath) && fs.existsSync(localEnvFilePath)) {
+        return projectPath === stateManager.getLocalEnvInfo(projectPath).projectPath;
+      }
     }
 
     return false;
